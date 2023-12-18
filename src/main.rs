c11066b76c51b088c6ea195af370352f202b0c54
Peregrine <peregrine@boum.org>
2023-12-18 23:00:51 +0100

    setup module, open window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
+pub mod setup;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: String::from("Minirogue"),
+                        ..Default::default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_plugins(TilemapPlugin)
+        .add_systems(Startup, crate::setup::setup::startup)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
 }
